732bcdceacce77f8af4aa977eb59f02b6256f3ef
hodifa <databaserepos@gmail.com>
2024-09-17 06:23:39 +0200

    fix uri in dashborad

--- dashboard/lib/uri.ts
+++ dashboard/lib/uri.ts
@@ -1,6 +1,6 @@
 const uri =
   process.env.NODE_ENV === "production"
-    ? "https://alethad-alwatani.onrender.com"
+    ? "https://alethad-alwatani.onrender.com/api/v1"
     : "http://localhost:10000/api/v1";
 
 export default uri;
